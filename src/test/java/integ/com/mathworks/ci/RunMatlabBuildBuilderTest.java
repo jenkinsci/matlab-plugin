5b98f51f05e108d238e0310b2880ca8112b6f17d
Vahila <70003902+Vahila@users.noreply.github.com>
2025-01-27 16:13:56 +0530

    Update RunMatlabBuildBuilderTest.java

--- src/test/java/integ/com/mathworks/ci/RunMatlabBuildBuilderTest.java
+++ src/test/java/integ/com/mathworks/ci/RunMatlabBuildBuilderTest.java
@@ -346,7 +346,7 @@ public class RunMatlabBuildBuilderTest {
         Combination c1 = new Combination(vals);
         MatrixRun build1 = matrixProject.scheduleBuild2(0).get().getRun(c1);
 
-        jenkins.assertLogContains("buildtool", build1);
+        jenkins.assertLogContains("buildtool check", build1);
         jenkins.assertBuildStatus(Result.FAILURE, build1);
 
         // Check for second Matrix combination
