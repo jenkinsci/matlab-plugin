d9bdeea587a3dd21a2ec7f58344b03da38f8d5e1
Sam Eagen <sameagen@vdi-ah2ddp-153.dhcp.mathworks.com>
2025-04-11 11:28:21 -0400

    First draft of parallel-safe results plugin

--- src/main/resources/+ciplugins/+jenkins/getDefaultPlugins.m
+++ src/main/resources/+ciplugins/+jenkins/getDefaultPlugins.m
@@ -6,9 +6,15 @@ arguments
     pluginProviderData (1,1) struct = struct();
 end
 
+if isMATLABReleaseOlderThan("R2025b")
+    reportPlugin = ciplugins.jenkins.BuildReportPlugin();
+else
+    reportPlugin = ciplugins.jenkins.ParallelizableBuildReportPlugin();
+end
+
 plugins = [ ...
     matlab.buildtool.internal.getFactoryDefaultPlugins(pluginProviderData) ...
-    ciplugins.jenkins.BuildReportPlugin() ...
+    reportPlugin ...
     ciplugins.jenkins.TaskRunProgressPlugin() ...
 ];
 end
